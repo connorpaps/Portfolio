3e052a3f97c5a406a301b9dac25a102f7b59632e
connorpaps <43863736+connorpaps@users.noreply.github.com>
2022-12-04 19:46:49 -0500

    updated hero resume

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -19,11 +19,11 @@ const Hero = () => (
         <br/>
         I'm a software developer that specializes in web technologies like React and JavaScript, along with game and backend development through langauges such as Python. I enjoy learning new languages and software while bringing real ideas to life. Below are some of my projects I have worked on over the past few months. I am currently open to work and can be contacted through LinkedIn or reached through the contact details below.
       </SectionText>
-      <a href="ConnorP_Resume.pdf" download>
-        <Button>
-          Download Resume
+      <Link href="ConnorP_Resume.pdf" download>
+      <Button>
+          View Resume
         </Button>
-      </a>
+        </Link>
     </LeftSection>
   </Section>
 );
